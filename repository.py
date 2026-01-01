9049278c2a3abda0d40e0e454c338f3546411902
alialfostovar <alialfostovar@gmail.com>
2026-01-01 13:25:22 +0330

    feat: implement generic BaseRepository pattern with CRUD operations

---
+++ repository.py
@@ -0,0 +1,75 @@
+from typing import Generic ,TypeVar, Type, Any, Sequence, Optional
+from pydantic import BaseModel
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.database import Base
+
+ModelType = TypeVar("ModelType", bound=Base)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+
+class BaseRepository(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    """
+    Base Repository with default CRUD operations.
+    """
+    def __init__(self, model: Type[ModelType], db: AsyncSession):
+        """
+        :param model: The SQLAlchemy model class (e.g., Users)
+        :param db: The AsyncSession dependency
+        """
+        self.model = model
+        self.db = db
+        
+    async def get(self, id: Any) -> Optional[ModelType]:
+        """Get a single record by ID."""
+        result = await self.db.execute(select(self.model).where(self.model.id == id))
+        record = result.scalars().first()
+        return record
+    
+    async def get_all(self, skip: int = 0, limit: int = 100) -> Sequence[ModelType]:
+        """Get all record with pagination."""
+        result = await self.db.execute(select(self.model).offset(skip).limit(limit))
+        records = result.scalars().all()
+        return records
+    
+    async def create(self, schema: CreateSchemaType) -> ModelType:
+        """Create a new record."""
+        new_record = self.model(**schema.model_dump())
+
+        self.db.add(new_record)
+        await self.db.commit()
+        await self.db.refresh(new_record)
+        
+        return new_record
+    
+    async def update(self, id:Any, schema: UpdateSchemaType) -> Optional[ModelType]:
+        """Update an existing record by ID."""
+        
+        record = await self.get(id)
+        if not record:
+            return None 
+        
+        update_data = schema.model_dump(exclude_unset=True)
+
+        for field, value in update_data.items():
+            setattr(record, field, value)
+            
+        self.db.add(record)
+        await self.db.commit()
+        await self.db.refresh(record)
+        
+        return record
+    
+    async def delete(self, id: Any) -> bool:
+        """Delete a record by ID."""
+        
+        record = await self.get(id)
+        if not record:
+            return False 
+        
+        await self.db.delete(record)
+        await self.db.commit()
+        
+        return True
+        
